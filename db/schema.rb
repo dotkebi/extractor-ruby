c82b6a43e9974b636e069d8a9c7e423a52b9f434
Myoungjin Ko <dotkebi@gmail.com>
2016-05-17 23:39:59 +0900

    example: model relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517143007) do
+ActiveRecord::Schema.define(version: 20160517143731) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string   "sequence"
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 20160517143007) do
     t.integer  "fifth"
     t.integer  "sixth"
     t.integer  "bonus"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bulletin_id"
   end
+
+  add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id"
 
 end
